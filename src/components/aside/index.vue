2904fc934f3e146a13f7f5009dd7261567e20704
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-12-29 20:06:06 +0500

    aside hover volor fix

--- src/components/aside/index.vue
+++ src/components/aside/index.vue
@@ -3,7 +3,7 @@
     <div class="aside--top">
       <div class="aside--top--profil" @click="profile">
         <div class="aside--top--profil--icon">
-        <img :src="ProfiLogo" alt="Profi logo" class="aside--top--profil--icon--img"  />
+          <img :src="ProfiLogo" alt="Profi logo" class="aside--top--profil--icon--img" />
         </div>
         <div class="aside--top--profil--name">
           <h3>Profi University <img :src="StarIcon" alt="star icon" /></h3>
@@ -11,35 +11,54 @@
         </div>
       </div>
       <div class="aside--top--list">
-        <router-link class="aside--top--list--item" v-for="(item ,index) in sidebarItems" :to="item?.to" :key="index">
-            <img :src="item?.icon" alt="icon" />
-            <span>{{ item?.title }}</span>
+        <router-link
+          class="aside--top--list--item"
+          v-for="(item, index) in sidebarItems"
+          :to="item?.to"
+          :key="index"
+        >
+          <span v-html="item?.icon"></span>
+          <p>{{ item?.title }}</p>
         </router-link>
       </div>
     </div>
     <div class="aside--bottom" @click="logout">
-      <img :src="Logout" alt="logout icon">
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M8.9 7.56023C9.21 3.96023 11.06 2.49023 15.11 2.49023H15.24C19.71 2.49023 21.5 4.28023 21.5 8.75023V15.2702C21.5 19.7402 19.71 21.5302 15.24 21.5302H15.11C11.09 21.5302 9.24 20.0802 8.91 16.5402M15 12.0002H3.62M5.85 8.65023L2.5 12.0002L5.85 15.3502"
+          stroke="currentColor"
+          stroke-width="1.5"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        />
+      </svg>
       <span>Chiqish</span>
     </div>
   </aside>
 </template>
 
 <script setup lang="ts">
-import {useRouter} from "vue-router";
+import { useRouter } from "vue-router";
 import ProfiLogo from "../../assets/icons/profi-logo.svg";
 import StarIcon from "../../assets/icons/stars-icon.svg";
 import Logout from "../../assets/icons/logout-icon.svg";
-import {sidebarItems} from "../../constants";
+import { sidebarItems } from "../../constants";
 
 const router = useRouter();
 
-const logout = ():void => {
-    router.push("/");
-}
+const logout = (): void => {
+  router.push("/");
+};
 
-const profile = ():void => {
-    router.push("/profile");
-}
+const profile = (): void => {
+  router.push("/profile");
+};
 </script>
 
 <style scoped lang="scss" src="./style.scss"></style>
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -7,27 +7,45 @@ import {type IsidebarItems} from '../types'
 
 export const sidebarItems :IsidebarItems[] = [{
     title: "Dashbord",
-    icon: DashbordIcon,
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <path d="M13.9914 3.19968L13.9916 3.19986L19.7516 7.2298C19.7516 7.22982 19.7516 7.22984 19.7516 7.22986C20.2208 7.55833 20.6651 8.0969 20.9925 8.72408C21.32 9.35129 21.5083 10.0244 21.5083 10.6002V17.3802C21.5083 19.654 19.6621 21.5002 17.3883 21.5002H6.60828C4.33583 21.5002 2.48828 19.6454 2.48828 17.3702V10.4702C2.48828 9.93574 2.65867 9.29667 2.95686 8.69105C3.25491 8.0857 3.65935 7.55706 4.0858 7.22443L4.0859 7.22435L9.09453 3.31542C9.09475 3.31525 9.09496 3.31508 9.09518 3.31491C10.432 2.27962 12.6011 2.22559 13.9914 3.19968ZM12.1485 16.6377L12.1509 16.6379C12.5065 16.6718 12.8544 16.5512 13.1118 16.2937L15.2465 14.1591C15.4659 14.4288 15.8009 14.6002 16.1783 14.6002C16.8444 14.6002 17.3783 14.0663 17.3783 13.4002V11.5402C17.3783 11.4148 17.3648 11.2496 17.288 11.0736C17.1665 10.7832 16.9299 10.5478 16.6387 10.4278L16.6227 10.4213L16.6064 10.4158C16.4873 10.3761 16.3411 10.3402 16.1783 10.3402H14.3183C13.6521 10.3402 13.1183 10.874 13.1183 11.5402C13.1183 11.9176 13.2896 12.2525 13.5594 12.472L12.4647 13.5667L11.7885 12.5591C11.5888 12.2398 11.2549 12.0557 10.9057 12.0224C10.5501 11.9886 10.2022 12.1092 9.94473 12.3666L6.96473 15.3466L6.96467 15.3466L6.95836 15.3531C6.50506 15.8232 6.49599 16.575 6.96473 17.0437C7.19335 17.2724 7.5027 17.3902 7.80828 17.3902C8.10805 17.3902 8.40936 17.2862 8.65183 17.0437L10.6019 15.0937L11.2811 16.1058C11.4703 16.3956 11.7922 16.6021 12.1485 16.6377Z" fill="currentColor" stroke="currentColor"/>
+  </svg>`,
     to: "/dashbord"
   },
   {
     title: "Chat",
-    icon: ChatIcon,
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <path d="M18.4698 16.83L18.8598 19.99C18.9598 20.82 18.0698 21.4 17.3598 20.97L13.1698 18.48C12.7098 18.48 12.2599 18.45 11.8199 18.39C12.5599 17.52 12.9998 16.42 12.9998 15.23C12.9998 12.39 10.5398 10.09 7.49985 10.09C6.33985 10.09 5.26985 10.42 4.37985 11C4.34985 10.75 4.33984 10.5 4.33984 10.24C4.33984 5.68999 8.28985 2 13.1698 2C18.0498 2 21.9998 5.68999 21.9998 10.24C21.9998 12.94 20.6098 15.33 18.4698 16.83Z" stroke="currentColor"  stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M13 15.2298C13 16.4198 12.56 17.5198 11.82 18.3898C10.83 19.5898 9.26 20.3598 7.5 20.3598L4.89 21.9098C4.45 22.1798 3.89 21.8098 3.95 21.2998L4.2 19.3298C2.86 18.3998 2 16.9098 2 15.2298C2 13.4698 2.94 11.9198 4.38 10.9998C5.27 10.4198 6.34 10.0898 7.5 10.0898C10.54 10.0898 13 12.3898 13 15.2298Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>`,
     to: "/chat"
   },
   {
     title: "Talabalar",
-    icon: StudentsIcon,
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <path d="M10.0495 2.52979L4.02953 6.45979C2.09953 7.71979 2.09953 10.5398 4.02953 11.7998L10.0495 15.7298C11.1295 16.4398 12.9095 16.4398 13.9895 15.7298L19.9795 11.7998C21.8995 10.5398 21.8995 7.72979 19.9795 6.46979L13.9895 2.53979C12.9095 1.81979 11.1295 1.81979 10.0495 2.52979Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M5.63109 13.0801L5.62109 17.7701C5.62109 19.0401 6.60109 20.4001 7.80109 20.8001L10.9911 21.8601C11.5411 22.0401 12.4511 22.0401 13.0111 21.8601L16.2011 20.8001C17.4011 20.4001 18.3811 19.0401 18.3811 17.7701V13.1301" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M21.3984 15V9" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>`,
     to: "/students"
   },
   {
     title: "Tadbirlar",
-    icon: EventsIcon,
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <path d="M12 15.5C14.21 15.5 16 13.71 16 11.5V6C16 3.79 14.21 2 12 2C9.79 2 8 3.79 8 6V11.5C8 13.71 9.79 15.5 12 15.5Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M4.35156 9.6499V11.3499C4.35156 15.5699 7.78156 18.9999 12.0016 18.9999C16.2216 18.9999 19.6516 15.5699 19.6516 11.3499V9.6499" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M10.6094 6.43012C11.5094 6.10012 12.4894 6.10012 13.3894 6.43012" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M11.1992 8.54982C11.7292 8.40982 12.2792 8.40982 12.8092 8.54982" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M12 19V22" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>`,
     to: "/events"
   },
   {
     title: "Sozlamalar",
-    icon: SettingsIcon,
+    icon: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
+    <path d="M3 9.10986V14.8799C3 16.9999 3 16.9999 5 18.3499L10.5 21.5299C11.33 22.0099 12.68 22.0099 13.5 21.5299L19 18.3499C21 16.9999 21 16.9999 21 14.8899V9.10986C21 6.99986 21 6.99986 19 5.64986L13.5 2.46986C12.68 1.98986 11.33 1.98986 10.5 2.46986L5 5.64986C3 6.99986 3 6.99986 3 9.10986Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>`,
     to: "/settings"
   }
 
