ead83cb35ea57f3fa958acae5fc6ded55df3a379
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-12-31 11:59:25 +0500

    tabel test

--- src/components/ui/table/index.vue
+++ src/components/ui/table/index.vue
@@ -1,20 +1,24 @@
 <template>
-    <table class="table">
-    <!-- Tabel heder -->
-        <tr class="table--heder">
-          <th class="table--heder--th">Ism-familya</th>
-          <th>Kurs</th>
-          <th>Ish statusi</th>
-          <th>Chat</th>
-        </tr>
-        <!-- Tabel body -->
-        <tr class="table--body">
-        <td class="table--body-td">Alisher Mo’minov Ne’mat o’g’li</td>
+    <!-- HTML -->
+  <table class="custom-table">
+    <thead>
+      <tr>
+        <th>Ism-familiya</th>
+        <th>Kurs</th>
+        <th>Ish statusi</th>
+        <th>Chat</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="el in 7">
+        <td>Alisher Mo'minov Ne'mat o'g'li</td>
         <td>4</td>
-        <td>Ish qidirmoqda</td>
-        <td><button>Chat <img :src="ChatIcon" alt="icon"></button></td>
-        </tr>
-    </table>
+        <td class="status">Ish qidirmoqda</td>
+        <td><button class="chat-btn">Chat <img class="chat-icon" :src="ChatIcon" alt="icon"/></button></td>
+      </tr>
+    </tbody>
+  </table>
+
 </template>
 
 <script setup lang="ts">
