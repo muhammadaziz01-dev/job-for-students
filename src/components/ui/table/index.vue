c1b18fc3e57b875e33f5560019975ac854abffee
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2024-12-31 11:19:54 +0500

    tabel start

--- src/components/ui/table/index.vue
+++ src/components/ui/table/index.vue
@@ -1,11 +1,24 @@
 <template>
-    <div>
-      <h1>Tabel</h1>
-    </div>
+    <table class="table">
+    <!-- Tabel heder -->
+        <tr class="table--heder">
+          <th class="table--heder--th">Ism-familya</th>
+          <th>Kurs</th>
+          <th>Ish statusi</th>
+          <th>Chat</th>
+        </tr>
+        <!-- Tabel body -->
+        <tr class="table--body">
+        <td class="table--body-td">Alisher Mo’minov Ne’mat o’g’li</td>
+        <td>4</td>
+        <td>Ish qidirmoqda</td>
+        <td><button>Chat <img :src="ChatIcon" alt="icon"></button></td>
+        </tr>
+    </table>
 </template>
 
 <script setup lang="ts">
-
+import ChatIcon from "../../../assets/icons/chat-icon.svg"
 </script>
 
 <style scoped lang="scss" src="./style.scss">
--- src/views/students/index.vue
+++ src/views/students/index.vue
@@ -1,11 +1,15 @@
 <template>
     <section class="student--wrapper">
       <StudentStatus/>
+      <div class="student--wrapper--parent">
+       <Table />
+      </div>
     </section>
 </template>
 
 <script setup lang="ts">
-import {StudentStatus} from "../../components/ui"
+import {StudentStatus , Table} from "../../components/ui"
+
 </script>
 
 <style scoped lang="scss" src="./style.scss">
