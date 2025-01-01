4745028bb2ffb25a2f5874f8f27733515ece7b9a
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2025-01-01 20:08:37 +0500

    fix babl size

--- src/main.ts
+++ src/main.ts
@@ -8,22 +8,24 @@ import App from './App.vue'
 // Router import
 import router from './router'
 
-// Toastify import 
+// Toastify import
 import 'vue3-toastify/dist/index.css';
 
-// AOS animation import 
-import 'aos/dist/aos.css'; 
-import AOS from 'aos';    
+// AOS animation import
+import 'aos/dist/aos.css';
+import AOS from 'aos';
 
-//  Antd import 
-import Antd from 'ant-design-vue';
+//  Antd import
+// import Antd from 'ant-design-vue';
+import { Pagination } from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
 
 AOS.init();
 
 
 const app = createApp(App)
-app.use(Antd);
+// app.use(Antd);
+app.use(Pagination)
 app.use(createPinia())
 app.use(router)
 
