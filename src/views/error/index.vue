f6bb5d4aa17213c654a1a8ac720650d269b42fe8
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2025-01-05 10:39:47 +0500

    error page cretet

--- src/views/error/index.vue
+++ src/views/error/index.vue
@@ -1,13 +1,36 @@
-<template>
-    <div>
-     <h1>Error paje</h1>
-    </div>
-</template>
-
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
+const router = useRouter();
 
+function goToSignIn() {
+  router.push('/');
+}
 </script>
 
-<style scoped lang="scss" >
+<template>
+    <section class="page_404">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12">
+          <div class="col-sm-10 col-sm-offset-1 text-center">
+            <div class="four_zero_four_bg">
+              <h1 class="text-center">404</h1>
+            </div>
+
+            <div class="contant_box_404">
+              <h3 class="h2">Look like you're lost</h3>
+
+              <p>the page you are looking for not avaible!</p>
+
+              <button @click="goToSignIn" class="link_404">Go to Sign In</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped lang="scss" src="./style.scss" >
 
 </style>
