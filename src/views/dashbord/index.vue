da9333d99006075ca9ee3b4a0b98983739d19ef4
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2025-01-11 15:23:57 +0500

    dashborrd page status card test

--- src/views/dashbord/index.vue
+++ src/views/dashbord/index.vue
@@ -4,7 +4,7 @@
      <div class="dashboard-content">
        <div class="dashboard-content--left">
           <div class="dashboard-content--left--status">
-             <StatusCardDashbord v-for="el in 3" :key="el"/>
+             <StatusCardDashbord v-for="el in dashStatusCardData" :key="el?.id" :date="el"/>
           </div>
        </div>
        <div class="dashboard-content--right"></div>
@@ -15,7 +15,8 @@
 
 <script setup lang="ts">
 import {Header} from "@/components"
-import {StatusCardDashbord} from "@/components/ui"
+import {StatusCardDashbord} from "@/components/ui";
+import {dashStatusCardData} from "@/constants"
 </script>
 
 <style scoped lang="scss" src="./style.scss">
