ed250baf06441425a5e7d2b36bb24edfafe65773
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2025-01-15 20:48:44 +0500

    events page updet

--- src/views/events/index.vue
+++ src/views/events/index.vue
@@ -1,8 +1,18 @@
 <template>
   <section class="event--wrapper">
     <div class="event--wrapper--top">
-    <h1>Tadbirlar</h1>
-    <MinniHeader/>
+      <h1>Tadbirlar</h1>
+      <MinniHeader />
+    </div>
+    <div class="event--wrapper--menu">
+      <div class="event--wrapper--menu--rout">
+        <router-link class="event--wrapper--menu--rout--link" to="/event/all">Barcha tadbirlar</router-link>
+        <router-link class="event--wrapper--menu--rout--link" to="/event/my">Tadbirlarim</router-link>
+      </div>
+      <button>+ Yaratish</button>
+    </div>
+    <div class="event--wrapper--bottom">
+      <router-view></router-view>
     </div>
   </section>
 </template>
@@ -11,6 +21,4 @@
 import MinniHeader from "@/components/header/min-header.vue";
 </script>
 
-<style scoped lang="scss" src="./style.scss">
-
-</style>
+<style scoped lang="scss" src="./style.scss"></style>
