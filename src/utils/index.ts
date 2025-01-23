96d90a3750317e77e24333709091556a9f7bf89f
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2025-01-23 07:29:11 +0500

    fix

---
+++ src/utils/index.ts
@@ -0,0 +1,5 @@
+//  Global functions  <---------------------
+// export const isActive = (routePath: string, path: string): boolean => {
+//   return routePath === path || routePath.startsWith(path);
+// };
+
