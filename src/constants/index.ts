11d8965861906b9a6d7512b9f2cc01890a4fa356
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2025-01-24 22:43:34 +0500

    fix

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -609,3 +609,43 @@ export const dataComponiStatus:IDataStudentStatus[] = [
     id:3,
   },
 ];
+
+// data event list
+export const eventsData = [
+  {
+    id: 1,
+    day: '28',
+    month: 'Okt',
+    title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    time: '09:00 - 12:30',
+    location: 'Online',
+    isEdit: false
+  },
+  {
+    id: 2,
+    day: '29',
+    month: 'Okt',
+    title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    time: '10:00 - 11:30',
+    location: '"Yoshlar Ishlari Agentligi" binosi',
+    isEdit: false
+  },
+  {
+    id: 3,
+    day: '01',
+    month: 'Noy',
+    title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    time: '14:20 - 15:40',
+    location: '"Yoshlar Ijod Saroyi" binosi',
+    isEdit: false
+  },
+  {
+    id: 4,
+    day: '04',
+    month: 'Noy',
+    title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+    time: '15:00 - 15:30',
+    location: 'Online',
+    isEdit: false
+  }
+];
--- src/views/events/all-events/index.vue
+++ src/views/events/all-events/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="evendCard--wrapper">
-      <div v-for="el in events" class="evendCard--wrapper--card" :key="el.id">
+      <div v-for="el in eventsData" class="evendCard--wrapper--card" :key="el.id">
          <div class="evendCard--wrapper--card--left">
             <h2 class="evendCard--wrapper--card--left--day">{{ el.day }} <span>{{ el.month }}</span> </h2>
          </div>
@@ -8,16 +8,16 @@
            <h2>{{ el?.title }}</h2>
            <div class="evendCard--wrapper--card--right--bottom">
              <span>
-               <img src="../../../assets/icons/time-icon.svg" alt="time icons">
+               <img :src="IconTime" alt="time icons">
                 {{ el?.time }}
              </span>
              <span>
-               <img src="../../../assets/icons/location-icon.svg" alt="location icons">
+               <img :src="IconLocation" alt="location icons">
                 {{ el?.location }}
              </span>
 
              <span v-if="el?.isEdit">
-               <img src="../../../assets/icons/edit-icon.svg" alt="calendar icons">
+               <img :src="IconEdit" alt="edit icons">
                <p>Tahrirlash</p>
              </span>
            </div>
@@ -29,45 +29,11 @@
 </template>
 
 <script setup lang="ts">
+import {eventsData} from "@/constants"
+import IconTime from "../../../assets/icons/time-icon.svg";
+import IconLocation from "../../../assets/icons/location-icon.svg";
+import IconEdit from "../../../assets/icons/edit-icon.svg";
 
-const events = [
-  {
-    id: 1,
-    day: '28',
-    month: 'Okt',
-    title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    time: '09:00 - 12:30',
-    location: 'Online',
-    isEdit: false
-  },
-  {
-    id: 2,
-    day: '29',
-    month: 'Okt',
-    title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    time: '10:00 - 11:30',
-    location: '"Yoshlar Ishlari Agentligi" binosi',
-    isEdit: false
-  },
-  {
-    id: 3,
-    day: '01',
-    month: 'Noy',
-    title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    time: '14:20 - 15:40',
-    location: '"Yoshlar Ijod Saroyi" binosi',
-    isEdit: false
-  },
-  {
-    id: 4,
-    day: '04',
-    month: 'Noy',
-    title: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
-    time: '15:00 - 15:30',
-    location: 'Online',
-    isEdit: false
-  }
-];
 </script>
 
 <style scoped lang="scss" src="./style.scss">
